8374a56d7519e378be8858952ee4fcc24bf9078e
Ayesh1998 <ayeshlak1998@gmail.com>
2020-03-26 08:16:10 +0530

    Added  delete icons to items

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const TodoItem = ({ item, pressHandler }) => {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.text}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={18} color="#333" />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
+  item: {
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
-    borderRadius: 10
+    borderRadius: 10,
+    flexDirection: "row"
+  },
+  text: {
+    marginLeft: 10
   }
 });
 
